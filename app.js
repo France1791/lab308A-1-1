31d83438f551e4b178e08fcbebbb8358f3a54a4d
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-06-11 14:44:15 -0400

    finished part 2 trampoline

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 
 
-let globalCounter = 0; 
-function increments(){
-    globalCounter++
-    console.log(globalCounter);
-    increments();
-}
+// let globalCounter = 0; 
+// function increments(){
+//     globalCounter++
+//     console.log(globalCounter);
+//     increments();
+// }
 
-try{
-    increments();
-}catch(error){
-    console.log("Maximum call stack size exceeded at: ",globalCounter)
-}
+// try{
+//     increments();
+// }catch(error){
+//     console.log("Maximum call stack size exceeded at: ",globalCounter)
+// }
 
+
+
+
+
+
+
+// part two 
+
+
+let testArray = [1,[2],[3,[7]]]
+let newArr=[]
+function trampoline(arr){
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            trampoline(arr[i])
+        }
+        else{newArr.push(arr[i])}
+    }
+    return newArr
+
+}
+console.log(trampoline(testArray))
 
